1837d8a116fadccf2c5a20975561b94a0092d425
Lourdes Magarín <lmagarin@gmail.com>
2015-06-08 18:04:24 +0200

    Preparando back-end
    Añado métodos para funcionalidad en controles y ventana

--- PrincipalGUI.java
+++ PrincipalGUI.java
@@ -11,6 +11,12 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class PrincipalGUI extends JDialog {
 
@@ -36,6 +42,14 @@ public class PrincipalGUI extends JDialog {
 	 * Create the dialog.
 	 */
 	public PrincipalGUI() {
+		setResizable(false);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				salir();
+			}
+		});
+		setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
 		setTitle("Conversor temperatura");
 		setBounds(100, 100, 450, 131);
 		contentPanel.setBounds(0, 0, 434, 58);
@@ -46,12 +60,22 @@ public class PrincipalGUI extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			{
 				JButton okButton = new JButton("Reset");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						reset();
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				JButton cancelButton = new JButton("Salir");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						salir();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
@@ -66,6 +90,13 @@ public class PrincipalGUI extends JDialog {
 		}
 		{
 			textFieldCelsius = new JTextField();
+			textFieldCelsius.setText("0");
+			textFieldCelsius.addFocusListener(new FocusAdapter() {
+				@Override
+				public void focusLost(FocusEvent arg0) {
+					aFahrenheit();
+				}
+			});
 			textFieldCelsius.setBounds(132, 10, 86, 20);
 			textFieldCelsius.setColumns(10);
 			contentPanel.add(textFieldCelsius);
@@ -76,10 +107,37 @@ public class PrincipalGUI extends JDialog {
 		contentPanel.add(lblFahrenheit);
 		{
 			textFieldFahrenheit = new JTextField();
+			textFieldFahrenheit.setText("32");
+			textFieldFahrenheit.addFocusListener(new FocusAdapter() {
+				@Override
+				public void focusLost(FocusEvent arg0) {
+					aCelsius();
+				}
+			});
 			textFieldFahrenheit.setBounds(336, 10, 86, 20);
 			contentPanel.add(textFieldFahrenheit);
 			textFieldFahrenheit.setColumns(10);
 		}
 		getContentPane().add(buttonPane);
+	}
+
+	protected void aCelsius() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void aFahrenheit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void salir() {
+		// TODO Auto-generated method stub
+		
 	}
 }
